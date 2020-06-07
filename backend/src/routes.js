2211627e5c78a6290573bfba508f52ecf198073b
Matheus Schneider <matheus.ssm@gmail.com>
2020-06-07 18:15:19 -0300

    Added Dev controller and database integration

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,8 @@
-import express from "express";
+const express = require("express");
+const DevController = require("./controllers/DevController");
 
 const routes = express.Router();
 
-routes.get("/", (req, res) => {
-  return res.send("Hello");
-});
+routes.post("/", DevController.store);
 
-export default routes;
+module.exports = routes;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,19 @@
-import express from "express";
-import routes from "./routes";
+const express = require("express");
+const mongoose = require("mongoose");
+
+const routes = require("./routes");
 
 const server = express();
+require("dotenv").config();
 
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-alt5r.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+  }
+);
+
+server.use(express.json());
 server.use(routes);
 
 server.listen(3333);
